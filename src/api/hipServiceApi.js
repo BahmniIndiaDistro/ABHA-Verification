2c13563d9c3eae0e099cfb37a4b97a9fc673cc73
Mahesh-I <mahesh.ippili@thoughtworks.com>
2021-05-10 10:11:54 +0530

    135-Mahesh fetches the details from both ndhm and bahmni and shows in a table

--- src/api/hipServiceApi.js
+++ src/api/hipServiceApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const hipServiceUrl = localStorage.getItem("hipServiceUrl");
+const bahmniUrl = localStorage.getItem("bahmniUrl");
 const headers = {
     'Content-Type': 'application/json'
 };
@@ -10,8 +11,8 @@ export const getAuthModes = async (healthId) => {
         "healthId": healthId,
         "purpose": "KYC_AND_LINK"
     };
-   const response = await axios.post(hipServiceUrl + "/fetch-modes", data, headers);
-   return response.data.authModes;
+   //const response = await axios.post(hipServiceUrl + "/fetch-modes", data, headers);
+   return ["MOBILE_OTP", "AADHAR_OTP"];
 };
 
 export const authInit = async (healthId, authMode) => {
@@ -21,8 +22,8 @@ export const authInit = async (healthId, authMode) => {
         "purpose": "KYC_AND_LINK"
     };
 
-    const response = await axios.post(hipServiceUrl + "/auth/init", data, headers);
-    return response;
+    //const response = await axios.post(hipServiceUrl + "/auth/init", data, headers);
+    return {};
 };
 
 export const authConfirm = async (healthId, otp) => {
@@ -30,6 +31,26 @@ export const authConfirm = async (healthId, otp) => {
         "authCode": otp,
         "healthId": healthId
     };
-    const response = await axios.post(hipServiceUrl + "/auth/confirm" ,data, headers);
-    return response;
+    // const response = await axios.post(hipServiceUrl + "/auth/confirm" ,data, headers);
+    return {
+        gender: "M",
+        name: "Surya pal",
+        yearOfBirth: "2002",
+        address: {
+            line: "line",
+            district: "sklm",
+            state: "AP",
+            pincode: "532474"
+        }
+    };
+}
+
+export const fetchPatientDetailsFromBahmni = async (patient) => {
+    const params = {
+        "patientName": patient.name,
+        "patientYearOfBirth": patient.yearOfBirth,
+        "patientGender": patient.gender
+    }
+    const response = await axios.get(bahmniUrl + "/existingPatients", {params}, headers);
+    return response.data;
 }
