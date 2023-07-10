f2de1b9840aaa0b49835ba9d81d6560380cc058c
SanoferSameera <sanofersameera25@gmail.com>
2023-07-10 18:03:20 +0530

    Add address label

--- src/components/patient-details/patientInfo.jsx
+++ src/components/patient-details/patientInfo.jsx
@@ -81,7 +81,7 @@ const PatientInfo = (props) => {
             <strong>{patient?.name?.replace(null,"")} </strong>
             (Age:<strong> {!isNaN(age) ? age : '-'} </strong>,
             Gender:<strong> {getPatientGender(patient?.gender) || '-'}</strong>)<br/>
-            {address.length !== 0 && <span>{address}<br/></span>}
+            {address.length !== 0 && <span>Address: {address}<br/></span>}
             Mobile: {patient?.phoneNumber || (patient?.identifiers != null ? patient?.identifiers[0]?.value : '-')}
             {patient?.id !== undefined && <span><br/>ABHA Address: {patient?.id}</span>}
             {patient?.healthId !== undefined && <span><br/>ABHA Address: {patient?.healthId}</span>}
