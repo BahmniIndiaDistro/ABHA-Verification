ba52eac18f4b62a214824b026ddcb50016b8da97
SanoferSameera <sanofersameera25@gmail.com>
2023-08-25 17:05:45 +0530

    BAH-3178 | Refactor. Extracted common function to a separate file

---
+++ src/components/Common/Util.jsx
@@ -0,0 +1,27 @@
+import {getDate} from "./DateUtil";
+
+export function mapPatient(patient){
+    var identifier = patient?.mobile !== undefined ? [{
+        value: patient.mobile
+    }] : undefined;
+    var address =  {
+        line: [patient?.address],
+        city: patient?.townName,
+        district: patient?.districtName,
+        state: patient?.stateName,
+        pincode: patient?.pincode
+    };
+    const ndhm = {
+        healthIdNumber: patient?.healthIdNumber,
+        id: patient?.healthId,
+        gender: patient.gender,
+        name: patient.name,
+        isBirthDateEstimated: patient?.birthdate !== undefined ? false : (patient?.monthOfBirth == null || patient?.dayOfBirth == null),
+        dateOfBirth:  patient?.birthdate === undefined  ? getDate(patient) : patient?.birthdate.split('-').reverse().join('-'),
+        address: address,
+        identifiers: identifier,
+        uuid: patient?.uuid
+    };
+    console.log(ndhm);
+    return ndhm;
+}
